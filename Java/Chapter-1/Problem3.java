e1974bf2139a35755b87de3c97dfdd32cdae4e74
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-02-26 20:44:42 +0530

    new file:   Java/Chapter-1/Problem3.java

---
+++ Java/Chapter-1/Problem3.java
@@ -0,0 +1,41 @@
+//import libraries
+
+public class Problem3 {
+    public static void main(String[] args) {
+        // declare variables
+
+        double h, w;
+        w = Math.pow(2, (-0.25 - 0.5 * 1)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 1)) * 1000;
+        System.out.printf("Size of A1: %dx%d\n", w, h);
+
+        w = Math.pow(2, (-0.25 - 0.5 * 2)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 2)) * 1000;
+        System.out.printf("Size of A2: %dx%d\n", w, h);
+
+        w = Math.pow(2, (-0.25 - 0.5 * 3)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 3)) * 1000;
+        System.out.printf("Size of A3: %dx%d\n", w, h);
+
+        w = Math.pow(2, (-0.25 - 0.5 * 4)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 4)) * 1000;
+        System.out.printf("Size of A4: %dx%d\n", w, h);
+
+        w = Math.pow(2, (-0.25 - 0.5 * 5)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 5)) * 1000;
+        System.out.printf("Size of A5: %dx%d\n", w, h);
+
+        w = Math.pow(2, (-0.25 - 0.5 * 6)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 6)) * 1000;
+        System.out.printf("Size of A6: %dx%d\n", w, h);
+
+        w = Math.pow(2, (-0.25 - 0.5 * 7)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 7)) * 1000;
+        System.out.printf("Size of A7: %dx%d\n", w, h);
+
+        w = Math.pow(2, (-0.25 - 0.5 * 8)) * 1000;
+        h = Math.pow(2, (0.25 - 0.5 * 8)) * 1000;
+        System.out.printf("Size of A8: %dx%d\n", w, h);
+
+    }
+}
