32ea04990c35c0cf96ff469558015b27d07928c0
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-02-27 11:42:47 +0530

    modified:   Java/Chapter-1/Problem3.java

--- Java/Chapter-1/Problem3.java
+++ Java/Chapter-1/Problem3.java
@@ -7,35 +7,35 @@ public class Problem3 {
         double h, w;
         w = Math.pow(2, (-0.25 - 0.5 * 1)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 1)) * 1000;
-        System.out.printf("Size of A1: %dx%d\n", w, h);
+        System.out.printf("Size of A1: %fx%f\n", w, h);
 
         w = Math.pow(2, (-0.25 - 0.5 * 2)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 2)) * 1000;
-        System.out.printf("Size of A2: %dx%d\n", w, h);
+        System.out.printf("Size of A2: %fx%f\n", w, h);
 
         w = Math.pow(2, (-0.25 - 0.5 * 3)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 3)) * 1000;
-        System.out.printf("Size of A3: %dx%d\n", w, h);
+        System.out.printf("Size of A3: %fx%f\n", w, h);
 
         w = Math.pow(2, (-0.25 - 0.5 * 4)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 4)) * 1000;
-        System.out.printf("Size of A4: %dx%d\n", w, h);
+        System.out.printf("Size of A4: %fx%f\n", w, h);
 
         w = Math.pow(2, (-0.25 - 0.5 * 5)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 5)) * 1000;
-        System.out.printf("Size of A5: %dx%d\n", w, h);
+        System.out.printf("Size of A5: %fx%f\n", w, h);
 
         w = Math.pow(2, (-0.25 - 0.5 * 6)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 6)) * 1000;
-        System.out.printf("Size of A6: %dx%d\n", w, h);
+        System.out.printf("Size of A6: %fx%f\n", w, h);
 
         w = Math.pow(2, (-0.25 - 0.5 * 7)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 7)) * 1000;
-        System.out.printf("Size of A7: %dx%d\n", w, h);
+        System.out.printf("Size of A7: %fx%f\n", w, h);
 
         w = Math.pow(2, (-0.25 - 0.5 * 8)) * 1000;
         h = Math.pow(2, (0.25 - 0.5 * 8)) * 1000;
-        System.out.printf("Size of A8: %dx%d\n", w, h);
+        System.out.printf("Size of A8: %fx%f\n", w, h);
 
     }
 }
