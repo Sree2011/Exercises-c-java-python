ad0b46c94af487e1814a05a88a3906a9091ec7ab
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-03-02 15:23:08 +0530

    new file:   Chapter-2/Python/Problem5.py

---
+++ Chapter-2/Python/Problem5.py
@@ -0,0 +1,9 @@
+import math as m
+a = float(input())
+print("Angle a is : %.3f"%a)
+print("SINE of angle a is : %.3f "%(m.sin(a)))
+print("COSINE of angle a is : %.3f "%(m.cos(a)))
+print("TANGENT of angle a is : %.3f "%(m.tan(a)))
+print("COTANGENT of angle a is : %.3f "%(1/m.tan(a)))
+print("SECANT of angle a is : %.3f "%(1/m.cos(a)))
+print("COSECANT of angle a is : %.3f "%(1/m.sin(a)))
