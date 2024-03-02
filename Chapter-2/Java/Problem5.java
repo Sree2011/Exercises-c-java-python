5968120f6eacb74f0b4f6dd356d72af0dfdbb15e
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-03-02 11:32:07 +0530

    modified:   Chapter-2/Java/Problem5.java

--- Chapter-2/Java/Problem5.java
+++ Chapter-2/Java/Problem5.java
@@ -10,12 +10,13 @@ public class Problem5 {
         a = sc.nextDouble();
 
         System.out.println("The trigonometric ratios of angle" + a + "are: ");
-        System.out.println("SINE of angle a is : " + Math.sin(a));
-        System.out.println("COSINE of angle a is : " + Math.cos(a));
-        System.out.println("TANGENT of angle a is : " + Math.tan(a));
-        System.out.println("COTANGENT of angle a is : " + 1 / Math.tan(a));
-        System.out.println("SECANT of angle a is : " + 1 / Math.cos(a));
-        System.out.println("COSECANT of angle a is : " + 1 / Math.sin(a));
+        System.out.printf("SINE of angle a is : %.3f\n", Math.sin(a));
+        System.out.printf("COSINE of angle a is : %.3f\n", Math.cos(a));
+        System.out.printf("TANGENT of angle a is : %.3f\n", Math.tan(a));
+        System.out.printf("COTANGENT of angle a is : %.3f\n", 1 / Math.tan(a));
+        System.out.printf("SECANT of angle a is : %.3f\n", 1 / Math.cos(a));
+        System.out.printf("COSECANT of angle a is : %.3f\n", 1 / Math.sin(a));
 
+        sc.close();
     }
 }
